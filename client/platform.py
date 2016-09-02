6e16a5df0a8ec0d0bb8eac9effb3820c6f635c31
Ben Fiola <bfiola@apple.com>
2016-09-01 17:54:15 -0700

    Removed framework files, added compiled SDL and SDL_ttf dylibs for Mac

--- client/platform.py
+++ client/platform.py
@@ -1,20 +1,36 @@
 import os, platform
 
 
-def init():
-    os.environ["PYSDL2_DLL_PATH"] = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib")
+class Platform(object):
+    def __init__(self, font_path):
+        self.font_path = font_path
+        self.configure_paths()
+
+    def configure_paths(self):
+        os.environ["PYSDL2_DLL_PATH"] = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lib")
+
+    def get_font_path(self, fontname):
+        return os.path.join(self.font_path, "%s.ttf" % fontname)
 
 
-def base_font_path():
-    p_str = platform.platform().lower()
-    if "windows" in p_str:
-        return "C:/Windows/Fonts"
-    elif "darwin" in p_str:
-        return "/Library/Fonts/"
+class WindowsPlatform(Platform):
+    def __init__(self):
+        super().__init__("C:/Windows/Fonts")
+
+
+class MacPlatform(Platform):
+    def __init__(self):
+        super().__init__("/Library/Fonts/")
+
+
+p_str = platform.platform().lower()
+_p = None
+if "windows" in p_str:
+    _p = WindowsPlatform()
+elif "darwin" in p_str:
+    _p = MacPlatform()
 
 
 def get_font_path(fontname):
-    return os.path.join(base_font_path(), "%s.ttf" % fontname)
-
-init()
+    return _p.get_font_path(fontname)
 
