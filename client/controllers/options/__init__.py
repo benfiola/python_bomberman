618cb9ef49915add771c8bb2d5cd8237e65acbb5
benfiola <benfiola@gmail.com>
2017-01-08 22:10:58 -0800

    Maybe views don't need to be separated from the controller - added more controllers

---
+++ client/controllers/options/__init__.py
@@ -0,0 +1,13 @@
+from client.controllers import *
+
+
+class OptionsController(Controller):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def set_up(self):
+        pass
+
+    def prepare_layout(self):
+        pass
+
